7c835223f046fcae12f92405ff0efba6012adef9
Pugsluv3r <217694036+Pugsluv3r@users.noreply.github.com>
2026-03-06 15:29:26 -0500

    Fixed for newest update

--- Advantageandsafety/Notifywhenlavaisnear.cs
+++ Advantageandsafety/Notifywhenlavaisnear.cs
@@ -1,25 +1,27 @@
-public static void NotifyWhenLavaIsNear()
-{
-    if (PhotonNetwork.InRoom)
-        try
+        public static float range = 5f;
+        public static float Notifdelay;
+        public static void NotifyWhenLavaIsNear()
         {
-            foreach (VRRig vRRig in GorillaParent.instance.vrrigs)
-                if (vRRig.isLocal && ((GorillaTagManager)GorillaGameManager.instance).isCurrentlyTag == false) 
+            if (PhotonNetwork.InRoom)
+                try
                 {
-                    float playerpos = UnityEngine.Vector3.Distance(vRRig.bodyTransform.position, GTPlayer.Instance.transform.position);
-                    if (playerpos < range)
-                        if (Time.time > Notifdelay)
-                            if (((GorillaTagManager)GorillaGameManager.instance).currentInfected.Contains(vRRig.Creator))
-                            {
-                                Notifdelay = Time.time + 2f;
-                                NotifiLib.SendNotification("<color=purple>[WARNING]</color> Lava near you");
-                            }
+                    foreach (VRRig vRRig in VRRigCache.ActiveRigs)
+                        if (vRRig.isLocal && ((GorillaTagManager)GorillaGameManager.instance).isCurrentlyTag == false) 
+                        {
+                            float playerpos = UnityEngine.Vector3.Distance(vRRig.bodyTransform.position, GTPlayer.Instance.transform.position);
+                            if (playerpos < range)
+                                if (Time.time > Notifdelay)
+                                    if (((GorillaTagManager)GorillaGameManager.instance).currentInfected.Contains(vRRig.Creator))
+                                    {
+                                        Notifdelay = Time.time + 2f;
+                                        NotifiLib.SendNotification("<color=purple>[WARNING]</color> Lava near you");
+                                    }
 
+                        }
+                    if (VRRig.LocalRig.isLocal &&((GorillaTagManager)GorillaGameManager.instance).isCurrentlyTag == false)
+                    {
+                        return;
+                        
+                    }
                 }
-            if (VRRig.LocalRig.isLocal &&((GorillaTagManager)GorillaGameManager.instance).isCurrentlyTag == false)
-            {
-                return;
-                
-            }
-        }
-        catch { } 
+                catch { }
